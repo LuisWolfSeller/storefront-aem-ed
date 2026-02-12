936ea5e896db7ef20f87dd2b969cf3d47935b2b5
luis carlos dominguez <luisdominguez@wolfsellers.com>
2026-02-12 13:59:15 -0600

    remove console.log

--- blocks/test-multi/test-multi.js
+++ blocks/test-multi/test-multi.js
@@ -1,6 +1,5 @@
 export default async function decorate(block) {
   const elements = block.children;
-  console.log(elements)
   let html = '';
   elements.array.forEach((element) => {
     html += element;
